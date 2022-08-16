8f9528dc154fdefd305c0c6f0e194b13bcc27ded
leochiubowser <leochiubowser@gmail.com>
2022-08-16 17:42:05 +0800

    delete character.js

--- game.js
+++ game.js
@@ -5,6 +5,27 @@ canvas.width = 410;
 const ground = 450;
 const gravity = 1;
 
+// Create Player
+
+class Player {
+    x = 175;
+    y = 250;
+    height = 25;
+    width = 25;
+    velocity = {
+        x: 0,
+        y: 1
+    }
+
+    draw() {
+        ctx.fillRect(this.x, this.y, this.height, this.width);
+    }
+}
+
+
+
+
+
 // Add Player
 var player = new Player();
 
