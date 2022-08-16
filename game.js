7e1496ad56a28e79835bc3253edb0d80ee937ffd
leochiubowser <leochiubowser@gmail.com>
2022-08-16 17:56:25 +0800

    jump

--- game.js
+++ game.js
@@ -1,9 +1,17 @@
+//Basic Setting
+
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.height = 500;
 canvas.width = 410;
 const ground = 450;
-const gravity = 1;
+const gravity = 0.5;
+var pressed = false;
+
+var situation = {
+    ready: true,
+    start: false,
+}
 
 // Create Player
 
@@ -30,15 +38,26 @@ class Player {
 var player = new Player();
 
 
-//Add Gravity
+//Move & add gravity 
 
 function move() {
 
     player.x += player.velocity.x;
 
     if (player.y + player.height + player.velocity.y <= ground) {
-        player.y += player.velocity.y;
-        player.velocity.y += gravity;
+
+        if (pressed) {
+            situation.ready = false;
+            situation.start = true;
+            player.velocity.y = -8;
+        }
+        else {
+            player.velocity.y += gravity;
+        }
+        if (situation.start) {
+            player.y += player.velocity.y;
+        }
+        pressed = false;
     }
     else {
         player.velocity.y = 0;
@@ -53,8 +72,13 @@ animation();
 
 function animation() {
     requestAnimationFrame(animation);
+    console.log(pressed);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     move();
     player.draw();
 }
+
+addEventListener("click", () => {
+    pressed = true;
+})
