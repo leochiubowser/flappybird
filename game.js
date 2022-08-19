05b455e88857e8b3afffa63fc3a8083d89fe4bea
leochiubowser <leochiubowser@gmail.com>
2022-08-19 11:04:03 +0800

    flaapy bird ver 1.2

--- game.js
+++ game.js
@@ -515,6 +515,41 @@ function showscore() {
 }
 
 
+//Auido 
+
+
+var point = new Audio();
+point.src = "./audio/point.wav";
+
+var ouch = new Audio();
+ouch.src = "./audio/hit.wav";
+
+var fly = new Audio();
+fly.src = "./audio/wing.wav";
+
+var die = new Audio();
+die.src = "./audio/die.wav";
+
+var originScore = 0;
+
+function playAudio() {
+    if (situation.start && pressed && !situation.end)
+        fly.play();
+
+    if (ouchSound && situation.end) {
+        ouch.play();
+        ouchSound = false;
+    }
+    if (dieSound && playdiesound) {
+        die.play();
+        dieSound = false;
+    }
+    if (score != originScore) {
+        point.play();
+        originScore = score;
+    }
+}
+
 
 
 //initlization game
@@ -568,7 +603,6 @@ function animation() {
 
 // Detect pressed space or click the mouse
 document.querySelector("body").addEventListener("click", (e) => {
-    fly.play();
     input.x = e.offsetX;
     input.y = e.offsetY;
     pressed = true;
